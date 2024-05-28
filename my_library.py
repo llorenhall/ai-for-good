3d2150ea5b309a0ea74de750c638749eadccc42a
llorenhall <166551076+llorenhall@users.noreply.github.com>
2024-05-28 10:18:06 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -35,3 +35,29 @@ def naive_bayes(table, evidence_row, target):
   #return your 2 results in a list
   return [neg, pos]
 
+def metrics(zipped_list):
+  #asserts here
+  assert isinstance(zipped_list, list), f'zipped_list is not a list. It is {type(zipped_list)}.'
+  assert all([isinstance(col, list) for col in zipped_list]), f'zipped_list is not a list of lists. {zipped_list=}.'
+  assert all((isinstance(item, (list, tuple)) and len(item) == 2) for item in zipped_list), f'zipped_list is not a list of pairs. {zipped_list=}.'
+  assert all(all(isinstance(val, int) and val >= 0 for val in item) for item in zipped_list), f'Each value in the pairs must be a non-negative integer. {zipped_list=}.'
+  #body of function below
+  #first compute the sum of all 4 cases. See code above
+  tn = sum([1 if pair==[0,0] else 0 for pair in zipped_list])
+  tp = sum([1 if pair==[1,1] else 0 for pair in zipped_list])
+  fp = sum([1 if pair==[1,0] else 0 for pair in zipped_list])
+  fn = sum([1 if pair==[0,1] else 0 for pair in zipped_list])
+
+  #now can compute precicision, recall, f1, accuracy. Watch for divide by 0.
+  precision = 0 if tp+fp == 0 else tp/(tp+fp) 
+  recall = 0 if tp+fn == 0 else tp/(tp+fn) 
+  f1 = 0 if precision + recall == 0 else 2 * (precision * recall)/(precision + recall)
+  accuracy = sum([1 if pair[0]==pair[1] else 0 for pair in zipped_list])/len(zipped_list)
+  #now build dictionary with the 4 measures
+  metrics_dict = {'Precision' : precision,
+                  'Recall' : recall,
+                  'F1' : f1,
+                  'Accuracy' : accuracy}
+  #finally, return the dictionary
+  return metrics_dict 
+
